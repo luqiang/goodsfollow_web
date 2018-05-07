9327b0ea3f88771937bae370cdd3029bfeb8f745
luqiang <lu_32000@163.com>
2018-05-07 21:28:11 +0800

    上传

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Layout } from 'antd';
+import 'antd/dist/antd.css'; 
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div style={{color:0xff0000}}>
+        <Layout>
+           <Header>Header</Header>
+       
+            
+               
+              <Content>Conten   
+
+                t</Content>
+              <Footer>Footer</Footer>
+            </Layout>
+         
+         
+
+      </div>            
+ 
+      
     );
   }
 }
